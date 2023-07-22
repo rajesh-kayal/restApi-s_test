e7683a66fc915ef10fadf44aec24266f6f58566f
Rajesh Kayal <119810174+rajesh-kayal@users.noreply.github.com>
2023-07-22 11:31:22 +0530

    Update web.php

--- web.php
+++ web.php
@@ -2,41 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-
-//controller namespace we are going to use
 use App\Http\Controllers\TodoController;
 
-Route::get('/todos',[TodoController::class,'getAllTodos']);
-Route::get('/todo/edit/{id}',[TodoController::class,'getTodoById']);
-Route::post('/todo/search',[TodoController::class,'getTodoByTitle']);
-Route::post('/todo/sort',[TodoController::class,'makeSort']);
-Route::post('/todo',[TodoController::class,'addTodo']);
-Route::any('/todo/{id}',[TodoController::class,'updateTodo']);
-Route::delete('/todo/delete/{id}',[TodoController::class,'deleteTodo']);
-
-use App\Http\Controllers\AjaxController;
-Route::get('/',[AjaxController::class,'index']);
-Route::get('/users',[AjaxController::class,'users']);
-
-use App\Http\Controllers\UserController;
-Route::post('/users/signup',[UserController::class,'signup']);
-Route::post('/users/signin',[UserController::class,'login']);
-Route::get('/users/logout',[UserController::class,'logout']);
-
-
-
-
-
-
-
+Route::get('/todos',[TodoController::class, 'getAll']);
+Route::get('/todo/{id}', [TodoController::class, 'TodoId']);
+Route::post('/todo/add/', [TodoController::class, 'insert']);
+Route::any('/todo/edit/{id}', [TodoController::class, 'edit']);
+Route::delete('/todo/delete/{id}', [TodoController::class, 'delete']);
