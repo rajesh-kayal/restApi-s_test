43d4d947aa9931fd86e7b6ed61652c3df4631819
Rajesh Kayal <119810174+rajesh-kayal@users.noreply.github.com>
2023-07-22 11:28:29 +0530

    Update TodoController.php

--- TodoController.php
+++ TodoController.php
@@ -3,103 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//using DB Facade
 use Illuminate\Support\Facades\DB;
-
 class TodoController extends Controller
 {
-    //
-
-    public function getAllTodos(){
-        $todos = DB::table('todos')->get();
-       // dd($todos);
-       //traditional php approach of converting php array into json object.
-       // return json_encode($todos);
-       //laravelic way of type casting into json object.
-       return response()->json($todos);
-
-      
+    public function getAll(){
+        $user=DB::table('todos')->get();
+        return response()->json($user);
     }
-
-    public function getTodoById($todo_id,Request $req){
-      if($req->isMethod('GET')){
-        $todo = DB::table('todos')->where('todo_id',$todo_id)->get();
-        //dd($todo[0]);
-        return response()->json($todo[0]);
-      }
+    public function TodoId($id){
+        $user=DB::table('todos')->where('todo_id',$id)->get();
+        return response()->json($user[0]);
     }
-    //getiing todo by its title.
-    public function getTodoByTitle(Request $req){
-              $title = $req->input('xyz');
-              $todos = DB::table('todos')
-                       ->where('title','like','%'.$title.'%')
-                       ->orWhere('description','like','%'.$title.'%')
-                       ->get();
-              return response()->json($todos);
-
+    public function insert(Request $req){
+        $affectedRows=DB::table('todos')->insert([
+            'title'     =>$req->input('t1'),
+            'description'=>$req->input('t2')
+        ]);
+        if($affectedRows){
+            return response()->json(['success' => 'data inserted successfull']);
+        }else{
+            return response()->json(['error' => 'data not inserted']);
+        }
     }
-
-    public function makeSort(Request $req){
-               $sortColumnName = $req->input('s1');
-               $sortType       = $req->input('s2');
-               $todos = DB::table('todos')->orderBy($sortColumnName,$sortType)->get();
-               return response()->json($todos);
-               
+    public function edit($id,Request $req){
+        if($req->isMethod('PUT') || $req->isMethod('PATCH')){
+            $affectedRows = DB::table('todos')->where('todo_id',$id)->update([
+                'title'     => $req->input('t1'),
+                'description' => $req->input('t2')
+            ]);
+            if ($affectedRows) {
+                return response()->json(['success' => 'data updated successfull']);
+            } else {
+                return response()->json(['error' => 'data not updated']);
+            }
+        }else{
+            return response()->json(['message'=>$req.__METHOD__.'is not supported']);
+        }
     }
-
-   //Data Add : POST
-    public function addTodo(Request $req){
-      //  dd($req->all());
-          $dataToInsert =[
-              'title'       => $req->input('title'),
-              'description' =>$req->input('desc')
-          ];
-          $affectedRows = DB::table('todos')->insert($dataToInsert);
-         if($affectedRows ==1)
-            return response()->json(['message'=>'One todo has been Added']);
-         else 
-            return response()->json(['message'=>'Unable to add todo']);
-
-
-
+    public function delete($id){
+        $affectedRows=DB::table('todos')->where('todo_id',$id)->delete();
+        if ($affectedRows) {
+            return response()->json(['success' => 'data deleted successfull']);
+        } else {
+            return response()->json(['error' => 'data not deleted']);
+        }
     }
-
-   //Data :put[single]/patch[Bulk]
-   public function updateTodo($todo_id,Request $req){
-         if($req->isMethod('PUT') || $req->isMethod('PATCH')){
-                
-            //dd($req->all());
-            $editTitle = $req->input('title');
-            $editDesc  = $req->input('description');
-            $editCreated=$req->input('created');
-
-            $affectedRows =DB::table('todos')->where('todo_id',$todo_id)->update([
-                          'title'      => $editTitle,
-                          'description'=>$editDesc,
-                          'created'=>  $editCreated
-                          ]);
-            if($affectedRows==1)
-               return response()->json(['message'=>'One todo Info has been Updated']);
-            else 
-               return response()->json(['message'=>'Unable to Update Now']);   
-
-
-         }else{
-            return response()->json(['message'=>$req->__METHOD__.' is not supported']);
-
-         }
-   }  
-
-   //DELETE: Method Data delete from Database
-     public function deleteTodo($todo_id){
-          $affectedRows =DB::table('todos')
-                           ->where('todo_id',$todo_id)
-                           ->delete();
-         if($affectedRows==1)
-            return response()->json(['message'=>'One Todo Info has been Deleted']);
-         else 
-            return response()->json(['message'=>'Unable to Delete Info']);
-
-
-     }
 }
